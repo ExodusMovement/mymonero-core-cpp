a671bb6dce42226b91cec617bd473f9af71a477d
andyvo <danh.vo@exodus.io>
2022-08-15 21:59:02 +0700

    Add more error msg in creating tx

--- src/monero_transfer_utils.hpp
+++ src/monero_transfer_utils.hpp
@@ -110,7 +110,7 @@ namespace monero_transfer_utils
 		tooManyDecoysRemaining			= 23,
 		needMoreMoneyThanFound			= 90
 	};
-	static inline string err_msg_from_err_code__create_transaction(CreateTransactionErrorCode code)
+	static inline const char *err_msg_from_err_code__create_transaction(CreateTransactionErrorCode code)
 	{
 		switch (code) {
 			case noError:
@@ -157,9 +157,14 @@ namespace monero_transfer_utils
 				return "Invalid payment ID";
 			case enteredAmountTooLow:
 				return "The amount you've entered is too low";
+			case notEnoughUsableDecoysFound:
+				return "Not enough usable decoys found";
+			case tooManyDecoysRemaining:
+				return "Too many unused decoys remaining";
 			case cantGetDecryptedMaskFromRCTHex:
 				return "Can't get decrypted mask from 'rct' hex";
 		}
+        return "Unknown error";
 	}
 	//
 	// See monero_send_routine for actual app-lvl interface used by lightwallets
